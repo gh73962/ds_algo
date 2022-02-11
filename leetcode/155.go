ca6b39c919fad9e16683b6dcde0a1165f4e587f9
me262 <gh73962@outlook.com>
2022-02-11 18:13:42 +0800

    s

---
+++ leetcode/155.go
@@ -0,0 +1,50 @@
+package leetcode
+
+// 输入：
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// 输出：
+// [null,null,null,null,-3,null,0,-2]
+
+// 解释：
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin();   --> 返回 -3.
+// minStack.pop();
+// minStack.top();      --> 返回 0.
+// minStack.getMin();   --> 返回 -2.
+
+type MinStack struct {
+}
+
+func Constructor() MinStack {
+
+}
+
+func (this *MinStack) Push(val int) {
+
+}
+
+func (this *MinStack) Pop() {
+
+}
+
+func (this *MinStack) Top() int {
+
+}
+
+func (this *MinStack) GetMin() int {
+
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
